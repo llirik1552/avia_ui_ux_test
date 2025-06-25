bf5b7ec6b103487ee3253b92aa3ff34c522e1fc0
llirik1552 <llirik1552@gmail.com>
2025-06-25 18:41:46 +0300

    добавил config pytest requirements

---
+++ config.py
@@ -0,0 +1,14 @@
+MAIN_URL = 'https://www.aviasales.ru/'
+MAIN_PAGE_TITLE = ("Купить авиабилеты дёшево онлайн "
+                   "| Авиасейлс — поиск самых дешевых билетов на самолет")
+
+API_SUGGESTED_URL = 'https://suggest.aviasales.com'
+API_SEARCH_URL = ''
+API_TICKETS_URL = 'https://tickets-api.aviasales.ru'
+API_TICKETS_NORTH_URL = 'https://tickets-api.eu-north-1.aviasales.ru'
+
+API_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/91.0.4472.124 Safari/537.36'
+}
